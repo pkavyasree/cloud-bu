4bd86ca26022f82e710840984af1fd6c53c77a61
kavyasree <kavya.panchumarthy@gmail.com>
2019-03-25 13:25:57 +0530

    fd

---
+++ src/aws-exports.ts
@@ -0,0 +1,7 @@
+export const awsmobile =  {
+    'aws_project_region': 'us-east-1',
+    'aws_cognito_identity_pool_id': 'us-east-1:3a437baf-4465-4700-a31e-0a6a6fd5dd00',
+    'aws_cognito_region': 'us-east-1',
+    'aws_user_pools_id': 'us-east-1_7smD0xjC5','aws_user_pools_web_client_id': '26es9c4f1cnnmm5e4sucsi2cjv'
+    };
+    
